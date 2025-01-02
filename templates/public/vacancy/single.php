e7a0c397c74e8757eafd78ca6ef370b36306ac2b
Bas <bas@alterio.nl>
2025-01-02 12:38:51 +0100

    BEM classes

--- templates/public/vacancy/single.php
+++ templates/public/vacancy/single.php
@@ -15,36 +15,36 @@ $type = $meta['_recruiterflow_type'][0] ?? '';
 $external_url = $meta['_recruiterflow_external_url'][0] ?? '';
 ?>
 
-<main class="vacancy">
-    <div class="vacancy__container">
-        <article <?php post_class('vacancy__article'); ?>>
-            <header class="vacancy__header">
-                <h1 class="vacancy__title"><?php the_title(); ?></h1>
+<main class="single-vacancy">
+    <div class="single-vacancy__container">
+        <article <?php post_class('single-vacancy__article'); ?>>
+            <header class="single-vacancy__header">
+                <h1 class="single-vacancy__title"><?php the_title(); ?></h1>
 
                 <?php if ($location || $type || $salary): ?>
-                    <div class="vacancy__meta">
+                    <div class="single-vacancy__meta">
                         <?php if ($location): ?>
-                            <span class="vacancy__meta-item vacancy__meta-item--location"><?php echo esc_html($location); ?></span>
+                            <span class="single-vacancy__meta-item single-vacancy__meta-item--location"><?php echo esc_html($location); ?></span>
                         <?php endif; ?>
 
                         <?php if ($type): ?>
-                            <span class="vacancy__meta-item vacancy__meta-item--type"><?php echo esc_html($type); ?></span>
+                            <span class="single-vacancy__meta-item single-vacancy__meta-item--type"><?php echo esc_html($type); ?></span>
                         <?php endif; ?>
 
                         <?php if ($salary): ?>
-                            <span class="vacancy__meta-item vacancy__meta-item--salary"><?php echo esc_html($salary); ?></span>
+                            <span class="single-vacancy__meta-item single-vacancy__meta-item--salary"><?php echo esc_html($salary); ?></span>
                         <?php endif; ?>
                     </div>
                 <?php endif; ?>
             </header>
 
-            <div class="vacancy__content">
+            <div class="single-vacancy__content">
                 <?php the_content(); ?>
             </div>
 
             <?php if ($external_url): ?>
-                <div class="vacancy__action">
-                    <a href="<?php echo esc_url($external_url); ?>" class="vacancy__button vacancy__button--apply" target="_blank" rel="noopener">
+                <div class="single-vacancy__action">
+                    <a href="<?php echo esc_url($external_url); ?>" class="single-vacancy__button single-vacancy__button--apply" target="_blank" rel="noopener">
                         <?php esc_html_e('Apply Now', 'wp-recruiterflow'); ?>
                     </a>
                 </div>
